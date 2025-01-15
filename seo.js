ca934d9ff69e23a1af4ffa76e546688ab5bb2ea5
Rameshkumar-V <vrameshkumar@60@gmail.com>
2025-01-15 11:40:24 +0530

    Settup all content Successfully

---
+++ seo.js
@@ -0,0 +1,21 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Rameshkumar V",
+  "jobTitle": "Python Developer",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "Trichirapalli",
+    "addressRegion": "Tamil Nadu",
+    "addressCountry": "India"
+  },
+  "description": "Experienced Python Developer specializing in Django, Flask, FastAPI, and web development.",
+  "sameAs": [
+    "https://github.com/rameshkumar-v",
+    "https://www.linkedin.com/in/rameshkumar-v",
+    "https://twitter.com/rameshkumar-v",
+    "https://rameshkumar-v.firebaseapp.com/"
+  ]
+}
+</script>
